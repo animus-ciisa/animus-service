1f93c560291d65ceddc772c4359ac00421ab170e
Matías Aguirre Luco <matiasaguirreluco@gmail.com>
2018-06-22 23:38:43 -0400

    Modificaciones hu6

---
+++ app/Data/Dao/UserDao.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matias
+ * Date: 22-06-18
+ * Time: 22:21
+ */
+
+namespace App\Data\Dao;
+
+
+class UserDao
+{
+
+}
---
+++ app/Data/Entities/UserEntity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matias
+ * Date: 22-06-18
+ * Time: 21:57
+ */
+
+namespace App\Data\Entities;
+
+
+class UserEntity
+{
+
+}
